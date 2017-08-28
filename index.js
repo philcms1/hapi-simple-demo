2a8d07007da2ed9a4ca187336ce65d43576bfdbf
philcms1 <phil@soaarchitects.com>
2017-08-28 11:44:35 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({port: 4000});
+
+server.route([{
+    method: 'GET',
+    path: '/api/questions',
+    handler: (request, reply) => {
+        const questions = [
+            'What is Hermione Granger\'s middle name?',
+            'For how long are house-elves required to serve their family?',
+            'What is Occlumency used for?',
+            'What is the name of the widely read daily newspaper, in Britain\'s wizard community?',
+            'Who is the editor of the Daily Prophet?'
+        ];
+
+        reply(questions);
+    }
+}]);
+
+server.start(() => {
+    console.log(`Server listening at: ${server.info.uri}`);
+});
